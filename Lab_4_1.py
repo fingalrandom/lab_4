e7c9dd9f5b089dbb157574642bfb7bcbcf6f7c66
fingalrandom <fingalrandom@gmail.com>
2015-01-12 12:41:39 +0700

    Update Lab_4_1.py

--- Lab_4_1.py
+++ Lab_4_1.py
@@ -2,34 +2,41 @@ import Lab_1
 import Lab_2
 import Lab_3
 import socket
-keyTXT = raw_input('Enter the name of vijiner key file(*.txt): ')
-key2TXT = raw_input('Enter the name of AES key file(*.txt): ')
-try:
-	keyFile = open(keyTXT,"rb")
-	key2File = open(key2TXT,"rb")
-except IOError:
-	print('File error')
-	exit()
-tmp = ""
-key = keyFile.read()
-key2 = key2File.read()
-sock = socket.socket()
-sock.bind(('', 1101))
-sock.listen(1)
-conn, addr = sock.accept()
-while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	tmp += data
-conn.close()
-dataFile = open('srv.bmp','wb')
-dataFile.write(tmp)
-dataFile.close()
-
-LSB = Lab_3.decrypt('srv.bmp')
-AES = Lab_2.crypt(LSB, key2, True)
-VIJ = Lab_1.decrypt(AES, key)
-rez = open('out.txt', 'wb')
-rez.write(VIJ)
-rez.close()
+import sys
+def main():
+	print "Example of main string for encode[-c]:"
+	print "[*.py] [vijiner key file name] [AES key file name]"
+	countPar = len(sys.argv)
+	if (countPar != 3):
+		print "There is wrong number of parameters."
+		exit()
+	keyFileName = sys.argv[1]
+	key2FileName = sys.argv[2]
+	try:
+		with open(keyFileName, 'rb') as keyFile:
+			key = keyFile.read()
+		with open(key2FileName, 'rb') as key2File:
+			key2 = key2File.read()
+	except IOError:
+		print('File error')
+		exit()
+	tmp = ""
+	sock = socket.socket()
+	sock.bind(('', 1101))
+	sock.listen(1)
+	conn, addr = sock.accept()
+	while True:
+		data = conn.recv(1024)
+		if not data:
+			break
+		tmp += data
+	conn.close()
+	with open('srv.bmp', 'wb') as dataFile:
+		dataFile.write(tmp)
+	LSB = Lab_3.decrypt('srv.bmp')
+	AES = Lab_2.crypt(LSB, key2, True)
+	VIJ = Lab_1.decrypt(AES, key)
+	with open('out.txt', 'wb') as rez:
+		rez.write(VIJ)
+if __name__ == "__main__":
+    main()
