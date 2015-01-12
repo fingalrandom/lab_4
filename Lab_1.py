36245da224176657cec9f1ce2a6d28da705c170f
fingalrandom <fingalrandom@gmail.com>
2015-01-12 12:40:35 +0700

    Update Lab_1.py

--- Lab_1.py
+++ Lab_1.py
@@ -1,3 +1,4 @@
+import sys
 def encrypt(text, key):
 	alph = ''
 	for i in range(256):
@@ -30,3 +31,33 @@ def decrypt(text, key):
 		decode = decode + alph[(ch-cur)%len(alph)]
 		i=i+1
 	return decode
+def main():
+	print "Example of main string:"
+	print "[*.py] [input file name] [key file name] [result file name] [encode or decode]"
+	print "	""-c"" - encode"
+	print "	""-d"" - decode"
+	countPar = len(sys.argv)
+	if (countPar != 5):
+		print "There is wrong number of parameters."
+		exit()
+	if(sys.argv[4] != "-c" and sys.argv[4] != "-d"):
+		print "irregular operation"
+		exit()
+
+	inFileName = sys.argv[1]
+	keyFileName = sys.argv[2]
+	rezFileName = sys.argv[3]
+	fl = sys.argv[4]
+
+	with open(inFileName, 'r') as inFile:
+		TXT = inFile.read()
+	with open(keyFileName, 'r') as keyFile:
+		KEY = keyFile.read()
+	if fl == '-c':
+		rez = encrypt(TXT, KEY)
+	else:
+		rez = decrypt(TXT, KEY)
+	with open(rezFileName, 'w') as rezFile:
+		rezFile.write(rez)
+if __name__ == "__main__":
+    main()
